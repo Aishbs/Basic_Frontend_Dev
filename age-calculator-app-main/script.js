8f43d8d50e03136a49c7209c53cc4700d016295e
Aishbs <bsaishwarya43@gmail.com>
2023-07-05 23:14:35 +0530

    Add calculation feature

---
+++ age-calculator-app-main/script.js
@@ -0,0 +1,62 @@
+let submit = document.querySelector(".svg");
+submit.addEventListener('click', hundleClick)
+
+function hundleClick() {
+    let dayValue = parseInt(document.querySelector("#day").value);
+    let monthValue = parseInt(document.querySelector("#month").value);
+    let yearValue = parseInt(document.querySelector("#year").value);
+    let alertDay = document.querySelector(".alert-message-day");
+    let alertMonth = document.querySelector(".alert-message-month");
+    let alertYear = document.querySelector(".alert-message-year");
+    let spanYear = document.querySelector(".year-display");
+    let spanMonth = document.querySelector(".month-display");
+    let spanDay = document.querySelector(".day-display");
+    let now = new Date()
+    let birthday = new Date(yearValue, monthValue - 1, dayValue)
+    if (isNaN(dayValue) || dayValue > 30) {
+        const daylabel = document.querySelector(".day-label");
+        let dayInput = document.querySelector("#day");
+        dayInput.style.border = "1px solid hsl(0, 100%, 67%)"
+        alertDay.textContent = "Must be a valid day"
+        daylabel.style.color = "hsl(0, 100%, 67%)"
+        setTimeout(() => {
+            alertDay.textContent = "";
+            daylabel.style.color = "hsl(0, 1%, 44%)"
+            dayInput.style.border = "1px solid hsl(0, 0%, 86%)"
+
+        }, 4000);
+    }
+    else if (isNaN(monthValue) || monthValue > 12) {
+        const monthlabel = document.querySelector(".month-label");
+        let monthInput = document.querySelector("#month");
+        monthInput.style.border = "1px solid hsl(0, 100%, 67%)"
+        monthlabel.style.color = "hsl(0, 100%, 67%)"
+        alertMonth.textContent = "Must be a valid month"
+    }
+    else if (yearValue > now.getFullYear()) {
+        let yearlabel = document.querySelector(".year-label");
+        let yearInput = document.querySelector("#day");
+        yearInput.style.border = "1px solid hsl(0, 100%, 67%)"
+        yearlabel.style.color = "hsl(0, 100%, 67%)"
+        alertYear.textContent = "Must be in the past"
+    }
+    else if (isNaN(yearValue)) {
+        let yearInput = document.querySelector("#year");
+        let yearlabel = document.querySelector(".label-year");
+        yearInput.style.border = "1px solid hsl(0, 100%, 67%)"
+        alertYear.textContent = "Must be a valid year"
+        yearlabel.style.color = "hsl(0, 100%, 67%)"
+    }
+    else {
+        let ageinmili = now.getTime() - birthday.getTime()
+        let ageInYears = Math.floor(ageinmili / (1000 * 60 * 60 * 24 * 365));
+        let ageInMonths = Math.floor(ageinmili / (1000 * 60 * 60 * 24 * 30));
+        const ageInDays = Math.floor(ageinmili / (1000 * 60 * 60 * 24));
+        console.log(ageInYears)
+        console.log(ageInMonths % 12)
+        console.log(ageinmili % 30)
+        spanDay.textContent = ageInDays % 30
+        spanMonth.textContent = ageInMonths % 12
+        spanYear.textContent = ageInYears
+    }
+}
